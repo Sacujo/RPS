4e0d0ba458e268eaf422adbdf102fcc69f8ac92c
Igor Guryan <sacujo@mail.ru>
2019-09-27 18:44:16 +0300

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by Igor Guryan on 27/09/2019.
+//  Copyright © 2019 Igor Guryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
